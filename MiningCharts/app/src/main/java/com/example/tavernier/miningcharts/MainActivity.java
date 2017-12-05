230d8e26a61a07231f73476df725ec1becdc55a6
DESKTOP-8VHS1B3\Tavernier <antoine.tavernier@epita.fr>
2017-12-05 10:41:51 +0100

    Layout Options + import color picker lib

--- MiningCharts/app/src/main/java/com/example/tavernier/miningcharts/MainActivity.java
+++ MiningCharts/app/src/main/java/com/example/tavernier/miningcharts/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tavernier.miningcharts;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -9,13 +12,15 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.PriorityQueue;
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback;
 
 public class MainActivity extends AppCompatActivity {
     private Button[] buttons = null;
     private WebView[] webViews = null;
     private AsyncExample[] asyncExamples= null;
     private String[] params = null;
+    private ColorPicker[] colorPickers = null;
     private int nbObjects = 2;
     private TextView[] textViews = null;
 
@@ -41,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
         asyncExamples = new AsyncExample[nbObjects];
 
+        colorPickers = new ColorPicker[nbObjects]; // plusieurs tableaux pour diff options?
+
         for (int i = 0; i < nbObjects; i++) {
             webViews[i].getSettings().setJavaScriptEnabled(true);
             webViews[i].setWebViewClient(new WebViewClient() {
@@ -52,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             //webViews[i].setVisibility(View.GONE);
             asyncExamples[i] = new AsyncExample(this, webViews[i], params[i], textViews[i]);
             asyncExamples[i].execute();
+            colorPickers[i] = new ColorPicker(MainActivity.this, 120, 60, 240);
             final int j = i;
             buttons[i].setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
@@ -60,6 +68,19 @@ public class MainActivity extends AppCompatActivity {
                     } else {
                         webViews[j].setVisibility(View.VISIBLE);
                     }
+                    colorPickers[j].show();
+                    colorPickers[j].setCallback(new ColorPickerCallback() {
+                        @Override
+                        public void onColorChosen(@ColorInt int color) {
+                            // Do whatever you want
+                            // Examples Integer.toString(Color.red(color))
+
+                            //Log.d("Pure Hex", Integer.toHexString(color));
+                            //Log.d("#Hex no alpha", String.format("#%06X", (0xFFFFFF & color)));
+                            //Log.d("#Hex with alpha", String.format("#%08X", (0xFFFFFFFF & color)));
+                            colorPickers[j].hide();
+                        }
+                    });
                 }
             });
         }
